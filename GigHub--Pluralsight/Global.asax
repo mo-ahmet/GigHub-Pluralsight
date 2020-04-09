7c58252c388fa3be8833a4a75b7ef2443c8ddfd1
Wingrao <56922415+Wingrao@users.noreply.github.com>
2020-04-09 10:00:35 +0200

    Implement 'Going To Gig' button

--- GigHub--Pluralsight/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GigHub__Pluralsight.MvcApplication" Language="C#" %>
---
+++ Gighub/Controllers/AttedancesController.cs
@@ -0,0 +1,37 @@
+﻿using Gighub.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Http;
+
+namespace Gighub.Controllers
+{
+    [Authorize]
+    public class AttedancesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public AttedancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        [HttpPost]
+        public IHttpActionResult Attend([FromBody]int gigId)
+        {
+            var userId = User.Identity.GetUserId();
+
+            if (_context.Attendances.Any(a => a.AttendeeId == userId && a.GigId == gigId))
+            {
+                return BadRequest("Already Exists.");
+            }
+
+            var attendance = new Attendance
+            {
+                GigId = gigId,
+                AttendeeId = userId
+            };
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
